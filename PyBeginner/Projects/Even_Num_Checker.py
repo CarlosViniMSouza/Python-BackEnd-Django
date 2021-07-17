6af3723a3cc87af29624e3ccc27c4396739842bf
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-17 13:37:37 -0300

    Project#1 made

---
+++ PyBeginner/Projects/Even_Num_Checker.py
@@ -0,0 +1,8 @@
+num = float(input("Input any value: "))
+
+if num % 2 == 0 :
+    print("This number is even!")
+else:
+    print("This number is odd!")
+
+# This is a simple project. Anyone can builder this!!
