d983133f33a7d1d5d53a06069426ee96d28627f0
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-18 00:19:56 -0300

    Second Project ...

--- PyBeginner/Projects/02_Basic_Calculator.py
+++ PyBeginner/Projects/02_Basic_Calculator.py
@@ -1,1 +1,31 @@
 # I'll start this project tomorrow.
+cond = "y"
+
+while cond == "Y" or cond == "y":
+
+    n1 = float(input("Write a number: "))
+
+    print("Operators: \n+ (sum) \n- (minus) \nx (product) \n/ (div)")
+    print("^ (expo) \nsqrt (square root) \nmod (modulus div)")
+
+    op = input("Write the operator: ")
+    n2 = float(input("Write a number: "))
+
+    if op == "+":
+        print("Your resp is = ", n1 + n2)
+    elif op == "-":
+        print("Your resp is = ", n1 - n2)
+    elif op == "x":
+        print("Your resp is = ", n1 * n2)
+    elif op == "/":
+        print("Your resp is = ", n1 / n2)
+    elif op == "^":
+        print("Your resp is = ", n1 ** n2)
+    elif op == "sqrt":
+        print("Your resp is = ", n1 ** 0.5)
+    elif op == "mod":
+        print("Your resp is = ", n1 % n2)
+    else:
+        print("Operation Impossible. Try again!")
+    resp = input("Do you want continue? [Y] or any letter: ")
+    cond = resp
