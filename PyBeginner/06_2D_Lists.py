2f0cb0edc55ec4986d96f7ecbd0f10df42f8aa81
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-17 21:19:49 -0300

    2d lists ok

---
+++ PyBeginner/06_2D_Lists.py
@@ -0,0 +1,15 @@
+# Well, here we doing 2 lists in 1 list. It's simple. Example:
+
+my_matrix = [
+    [0, 3, 9],
+    [2, 4, 6],
+    [10, 12, 14]
+]
+
+print(my_matrix) # See the compressed list.
+
+print(my_matrix[0][0]) # Accessing the index of matrix.
+
+# We can use For Loops to run 'my_matrix'
+for cont in my_matrix:
+    print(cont)
