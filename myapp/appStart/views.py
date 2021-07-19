86439f98c3f4c1a18a825f069e0056403c0fbf06
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-19 17:53:53 -0300

    urls_routes and django_apps add

--- myapp/appStart/views.py
+++ myapp/appStart/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1> Hello, be welcome to my Site! </h1>')
+    return render(request, 'index.html')
 
 # Okay, now, lets go to the Terminal writer 'python manage.py runserver' for generate the application's localhost
