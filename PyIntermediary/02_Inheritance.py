7e1a785353255f04e9ba74d5e8749bf1f6450df0
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-18 22:46:27 -0300

    Inheritance add

---
+++ PyIntermediary/02_Inheritance.py
@@ -0,0 +1,9 @@
+# Created the file 'File_Student' with a class that will be pushed until here
+
+from File_Student import Student
+
+class People(Student):
+    pass
+
+register = People()
+print(register.name, "\n", register.salary)
---
+++ PyIntermediary/File_Student.py
@@ -0,0 +1,5 @@
+class Student:
+    name = "Carlos Souza"
+    age = 20
+    gender = "M"
+    salary = 5000.5
