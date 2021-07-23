056c5af7c3bf1650be492cac40a966aaab389459
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-23 18:51:45 -0300

    re-build page static fail

--- myapp/appStart/models.py
+++ myapp/appStart/models.py
@@ -1,3 +1,14 @@
+# Templates are a Django framework that allows us to perform operations on databases without
+# having to code in SQL or use third-party software to do so.
+
+# But to make use of this, it is necessary to have knowledge of Classes and Inheritance
+# in Python to perform the modeling.
+
 from django.db import models
 
 # Create your models here.
+
+class Feature:
+    id: int
+    name: str
+    details: str
--- myapp/appStart/views.py
+++ myapp/appStart/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    user = {
-        'name': 'CarlosViniMSouza',
-        'age': 20,
-        'nationality': 'Brazilian',
-        'college': 'Graduate'
-    }
-    return render(request, 'index.html', user)
+    # feature1 = Feature()
+    # feature1.id = 0
+    # feature1.name = "Django"
+    # feature1.details = "Service in maintenance"
+    return render(request, 'index.html') # {'feature': feature1}
 
 # Okay, now, lets go to the Terminal writer 'python manage.py runserver' for generate the application's localhost
 
