e0da4769a4f2fbfa329f76ad629858a2884c7ce1
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-31 22:28:33 -0300

    key deleted

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6bx_a&7ziw*y3w08)3qp=9qk-p!@-j0)52(63@=6lwy5)^44!'
+SECRET_KEY = 'django-insecure-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
